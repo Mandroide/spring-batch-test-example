ecdb244aaf1200443bc18c5faf4870cbe3739d0c
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2023-12-11 20:48:24 -0400

    Add main entry point

---
+++ src/main/java/com/example/SpringBatchTestExampleApp.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTestExampleApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchTestExampleApp.class, args);
+    }
+}
